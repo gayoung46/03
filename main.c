a54dcdd562c2623e91d4a0b0444713a120788a84
gayoung46 <gayoung04066@naver.com>
2023-09-22 01:28:42 +0900

    4

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) 
+int main(void)
 {
-  char c;
-  printf("input a character :");
-  scanf("%c", &c);
-  
-  printf("The next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
-  
-   system("PAUSE");	
-
+    int a;
+    int b;
+    (float) a/b; 
+    
+    printf("분자를 입력하세요: ");
+    scanf("%d", &a);
+    
+    printf("분모를 입력하세요 : ");
+    scanf("%d", &b);
+    
+    printf("나누기의 결과는%f 입니다.\n ", (float) a/b); 
+     
+  system("PAUSE");	
   return 0;
 }
